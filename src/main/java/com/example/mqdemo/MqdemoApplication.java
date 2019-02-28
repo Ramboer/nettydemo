cf0bf6c25f8ed83d2049d3eaaff0387ff680d2c0
11094322 <442065090@qq.com>
2019-02-28 14:11:44 +0800

    remove unused import.

--- src/main/java/com/example/mqdemo/MqdemoApplication.java
+++ src/main/java/com/example/mqdemo/MqdemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.mqdemo;
 
-import com.example.mqdemo.client.Client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
